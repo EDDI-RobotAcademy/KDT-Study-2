6d6a9e82aea79b1fb66a4b4af5b2a40c462ed577
Youngjin Shin <mail@jindev.me>
2023-04-28 22:44:41 +0900

    feat: [신영진] Github actions publish 추가

--- Vue/frontend/vue.config.js
+++ Vue/frontend/vue.config.js
@@ -6,5 +6,8 @@ module.exports = defineConfig({
   ],
   chainWebpack: config => {
     config.module.rules.delete('eslint');
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/KDT-Study-2/'
+    : '/'
 })
