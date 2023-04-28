ea1b35bd7e14b08e4021a7f86a8cad1ffe248b07
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-04-28 16:23:53 +0900

    Update actions.js

--- Vue/frontend/src/store/product/actions.js
+++ Vue/frontend/src/store/product/actions.js
@@ -1,19 +1,18 @@
 import {
-    REQUEST_PRODUCT_LIST_TO_SPRING,
-    REQUEST_PRODUCT_TO_SPRING,
+  REQUEST_PRODUCT_LIST_TO_SPRING,
+  REQUEST_PRODUCT_TO_SPRING,
 } from "./mutation-types";
 
 import axiosInst from "@/utility/axiosInst";
 
 export default {
-    requestProductListoSpring({ commit }) {
-        return axiosInst.get('/product/list')
-            .then((res) => {
-                commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data)
-            })
-    },
-    requestCreateProductToSpring({}, payload) {
-        const { name, price, company, manufactureDate, category } = payload;
+  requestProductListoSpring({ commit }) {
+    return axiosInst.get("/product/list").then((res) => {
+      commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data);
+    });
+  },
+  requestCreateProductToSpring({}, payload) {
+    const { name, price, company, manufactureDate, category } = payload;
 
     return axiosInst
       .post("/product/register", {
@@ -31,13 +30,14 @@ export default {
         alert("문제 발생!");
       });
   },
-  requestDeleteProductToSpring ({}, productId) {
-        return axiosInst.delete(`/jpa-product/${productId}`)
-            .then((res) => {
-                alert('삭제 성공!')
-            })
-            .catch(() => {
-                alert('문제 발생!')
-            })
-    },
+  requestDeleteProductToSpring({}, productId) {
+    return axiosInst
+      .delete(`/jpa-product/${productId}`)
+      .then((res) => {
+        alert("삭제 성공!");
+      })
+      .catch(() => {
+        alert("문제 발생!");
+      });
+  },
 };
