9d0f324ad4c46643ee2e2b9f07a223e6c0c583d4
wlsdbwls <wlsdbwls614@gmail.com>
2023-04-28 14:45:00 +0900

    feat: [진유진] 상품 리스트 UI 기능 추가 [PRODUCT_LIST_PAGE-1]

--- Vue/frontend/src/store/product/actions.js
+++ Vue/frontend/src/store/product/actions.js
@@ -1,28 +1,34 @@
 import {
-  REQUEST_PRODUCT_LIST_TO_SPRING,
-  REQUEST_PRODUCT_TO_SPRING,
+    REQUEST_PRODUCT_LIST_TO_SPRING,
+    REQUEST_PRODUCT_TO_SPRING,
 } from "./mutation-types";
 
 import axiosInst from "@/utility/axiosInst";
 
 export default {
-  requestCreateProductToSpring({}, payload) {
-    const { name, price, company, manufactureDate, category } = payload;
+    requestProductListoSpring({ commit }) {
+        return axiosInst.get('/product/list')
+            .then((res) => {
+                commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data)
+            })
+    },
+    requestCreateProductToSpring({}, payload) {
+        const { name, price, company, manufactureDate, category } = payload;
 
-    return axiosInst
-      .post("/product/register", {
-        name,
-        price,
-        company,
-        manufactureDate,
-        category,
-      })
-      .then((res) => {
-        alert("상품 등록 성공!");
-        return res;
-      })
-      .catch(() => {
-        alert("문제 발생!");
-      });
-  },
+        return axiosInst
+            .post("/product/register", {
+                name,
+                price,
+                company,
+                manufactureDate,
+                category,
+            })
+            .then((res) => {
+                alert("상품 등록 성공!");
+                return res;
+            })
+            .catch(() => {
+                alert("문제 발생!");
+            });
+    },
 };
--- Vue/frontend/src/views/HomeView.vue
+++ Vue/frontend/src/views/HomeView.vue
@@ -1,15 +1,23 @@
 <template>
-  <hello-world />
+  <v-container>
+    <div>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/product-list-page">상품 게시판</router-link> |
+      </nav>
+    </div>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    HelloWorld,
+  },
+}
 </script>
