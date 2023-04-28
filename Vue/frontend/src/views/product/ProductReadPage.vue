37797d897990efe89513615347ae9523ae285104
cluypkoj <cluypkoj68@gmail.com>
2023-04-28 17:42:20 +0900

    feat:[백지우] 상품 읽기 frontend 작성 [VPR-READ-1] [

---
+++ Vue/frontend/src/views/product/ProductReadPage.vue
@@ -0,0 +1,52 @@
+<template lang="">
+    <div>
+        <h2>상품 게시판</h2>
+        <board-read-form v-if="products" :product="products"/>
+        <p v-else>로딩중 .......</p>
+         <router-link :to="{ name: 'ProductModifyPage', params: { productId }}">
+        <router-link :to="{ name: 'ProductModifyPage', params: { productId }}">
+            게시물 수정
+        </router-link> 
+        </router-link>
+        <button @click="onDelete">삭제</button>
+        <router-link :to="{ name: 'ProductListPage' }">
+            돌아가기
+        </router-link>
+    </div>
+</template>
+<script>
+
+import ProductReadForm from '@/components/product/ProductReadForm.vue'
+import { mapActions, mapState } from 'vuex';
+
+const productModule = 'productModule'
+export default {
+    components: {
+        ProductReadForm
+    },
+    props: {
+        ProductId: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        ...mapState(productModule, ['product'])
+    },
+    methods: {
+        ...mapActions(
+            productModule, ['requestproductToSpring', 'requestDeleteproductToSpring']
+        ),
+        async onDelete () {
+            await this.requestProductToSpring(this.productId)
+            await this.$router.push({ name: 'ProductListPage' })
+        }
+    },
+    created () {
+        this.requestproductToSpring(this.productId)
+    }
+}
+</script>
+<style lang="">
+    
+</style>
