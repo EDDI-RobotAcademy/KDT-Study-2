fd875b78890cd0e02470499932974406adea56ef
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-04-29 21:52:50 +0900

    feat:[김진호] 상품목록 삭제 Backend완료[SPD-1]

--- Spring/src/main/java/com/example/demo/product/repository/JpaProductRepository.java
+++ Spring/src/main/java/com/example/demo/product/repository/JpaProductRepository.java
@@ -4,5 +4,4 @@ import com.example.demo.product.entity.JpaProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductRepository  extends JpaRepository<JpaProduct, Long> {
-}
- 
+} 
