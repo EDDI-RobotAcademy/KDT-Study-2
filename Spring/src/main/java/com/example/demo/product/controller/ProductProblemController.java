09cf46ef77793311190c46153692f1829b5480f1
cluypkoj <128336184+cluypkoj@users.noreply.github.com>
2023-04-30 15:19:33 +0900

    feat:[백지우] 상품목록 읽기 Backend [SPD-READ-1]수정[

--- Spring/src/main/java/com/example/demo/product/controller/ProductProblemController.java
+++ Spring/src/main/java/com/example/demo/product/controller/ProductProblemController.java
@@ -1,17 +1,13 @@
 package com.example.demo.product.controller;
 
-
-
 import com.example.demo.product.controller.form.RequestProductForm;
 import com.example.demo.product.entity.ProductEntity;
 import com.example.demo.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +16,25 @@ public class ProductProblemController {
 
     final private ProductService productService;
 
+
+
     @PostMapping("/register")
-    public ProductEntity registerProduct(@RequestBody RequestProductForm requestProductForm){
+    public ProductEntity registerProduct(@RequestBody RequestProductForm requestProductForm) {
         log.info("registerProduct()");
 
         return productService.register(requestProductForm.toProductEntity());
     }
 
+    @GetMapping("/{productId}")
+    public ProductEntity readProduct(@PathVariable("productId") Long productId) {
+        log.info("productRead()");
+        return productService.read(productId);
+    }
 
+    @DeleteMapping("/{productId}")
+    public void deleteProduct(@PathVariable("productId") Long productId) {
+        log.info("productRead()");
+
+        productService.delete(productId);
+    }
 }
--- Spring/src/main/java/com/example/demo/product/service/ProductServiceImpl.java
+++ Spring/src/main/java/com/example/demo/product/service/ProductServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.product.service;
 
-
 import com.example.demo.product.entity.ProductEntity;
 import com.example.demo.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +9,24 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
     final private ProductRepository productRepository;
+
+    @Override
+    public ProductEntity read(Long productId) {
+        Optional<ProductEntity> maybeProductEntity = productRepository.findById(productId);
+
+        if(maybeProductEntity.isEmpty()) {
+            log.info("정보가 없습니다!");
+            return null;
+        }
+    }
+
+    @Override
+    public void delete(Long productId) {
+        productRepository.deleteById(productId);
+    }
 
     @Override
     public ProductEntity register(ProductEntity productEntity) {
